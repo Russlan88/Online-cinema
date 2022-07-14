39534116145bb0de83b72c80f7e3bae544d84b85
ruslan ogarkov <ruslan.ogarkov@aalto.it>
2022-07-14 10:23:09 +0300

    remove all files

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 [] setting invaironment
 
 yarn add @nestjs/config @typegoose/typegoose mongoose nestjs-typegoose
-@nestjs/jwt
+@nestjs/jwt passport-jwt @nestjs/passport @nestjs/passport class-validator
 
 yarn add -D @types/mongoose
+
+env
--- backend/src/config/jwt.config.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { JwtModuleOptions } from '@nestjs/jwt';
-import { TypegooseModuleOptions } from 'nestjs-typegoose';
-
-export const getJwtConfig = async (
-	confiService: ConfigService,
-): Promise<JwtModuleOptions> => ({
-	secret: confiService.get('JWT_SECRET'),
-});

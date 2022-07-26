2d11aa010c2579fb22650b971992fcb7581d24b2
ruslan ogarkov <ruslan.ogarkov@aalto.it>
2022-07-26 19:01:53 +0300

    add movie controller and dto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,6 +12,7 @@ import { TypegooseModule } from 'nestjs-typegoose';
 import { GenreModule } from './genre/genre.module';
 import { FilesModule } from './files/files.module';
 import { ActorModule } from './actor/actor.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
 	imports: [
@@ -26,6 +27,7 @@ import { ActorModule } from './actor/actor.module';
 		GenreModule,
 		FilesModule,
 		ActorModule,
+		MovieModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
---
+++ backend/src/movie/movie.model.ts
@@ -0,0 +1,53 @@
+import { prop, Ref } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { IsNumber, IsString } from 'class-validator';
+import { ActorModel } from 'src/actor/actor.model';
+import { GenreModel } from 'src/genre/genre.model';
+
+export interface MovieModel extends Base {}
+
+export class Parameters {
+	@prop()
+	year: number;
+
+	@prop()
+	duration: number;
+
+	@prop()
+	country: number;
+}
+
+export class MovieModel extends TimeStamps {
+	@prop()
+	poster: string;
+
+	@prop()
+	bigPoster: string;
+
+	@prop({ unique: true })
+	title: string;
+
+	@prop()
+	parameters: Parameters;
+
+	@prop({ default: 4.0 })
+	rating?: number;
+
+	@prop({ ref: () => GenreModel })
+	genres: Ref<GenreModel>[];
+
+	@prop({ default: 0 })
+	countOpened?: number;
+
+	@prop({ unique: true })
+	videoUrl: string;
+
+	@prop({ ref: () => ActorModel })
+	actors: Ref<ActorModel>[];
+
+	@prop({ unique: true })
+	slug: string;
+
+	@prop({ default: false })
+	isSendTelegram?: boolean;
+}

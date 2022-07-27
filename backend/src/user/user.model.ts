3bb4e71fb14ec3d0c7643a3982004695185cc446
ruslan ogarkov <ruslan.ogarkov@aalto.it>
2022-07-27 14:26:22 +0300

    add favorites

--- backend/src/user/user.model.ts
+++ backend/src/user/user.model.ts
@@ -1,5 +1,6 @@
 import { prop, Ref } from '@typegoose/typegoose';
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+import { MovieModel } from 'src/movie/movie.model';
 
 export interface UserModel extends Base {}
 
@@ -12,4 +13,7 @@ export class UserModel extends TimeStamps {
 
 	@prop({ default: false })
 	public isAdmin?: boolean;
+
+	@prop({ default: [], ref: () => MovieModel })
+	favorites?: Ref<MovieModel>[];
 }
